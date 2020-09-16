aadc76678457766182479de6ca4c706e9dc1b77d
Santiago Marulanda Molina <santiagomm1997@gmail.com>
2020-09-16 00:28:06 -0500

    Update REAMDE.md and settings.example.php

--- web/sites/default/settings.example.php
+++ web/sites/default/settings.example.php
@@ -775,10 +775,10 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 }
 
 $databases['default']['default'] = [
-  'database' => '',
+  'database' => 'el_celler_del_duc',
   'username' => '',
   'password' => '',
-  'prefix' => '',
+  'prefix' => 'cdd_',
   'host' => 'localhost',
   'port' => '3306',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
